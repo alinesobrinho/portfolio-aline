c6c40eecfada61256ce05bb45c3bfe994972646b
alinesobrinho <alinesobrinho11@gmail.com>
2026-07-24 12:14:58 -0300

    portfolio

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Navbar } from "./core/layout/navbar/navbar";
+import { Home } from "./features/home/home";
+import { Footer } from "./core/layout/footer/footer";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Navbar, Home, Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })

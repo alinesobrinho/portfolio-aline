15baaaefea451d877e6dff3a674513199b43d658
alinesobrinho <alinesobrinho11@gmail.com>
2026-08-16 16:38:19 -0300

    Alteracao navbar
    Alteração na barra de navegação para ela ficar selecionada quando estiver na pagina selecionada

--- src/app/core/layout/navbar/navbar.ts
+++ src/app/core/layout/navbar/navbar.ts
@@ -1,11 +1,85 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  HostListener
+} from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+
+  standalone: true,
+
   templateUrl: './navbar.html',
-  styleUrl: './navbar.scss',
+
+  styleUrl: './navbar.scss'
 })
 export class Navbar {
+
+  activeSection: string = 'home';
+
+
+  // =========================
+  // CLIQUE NO MENU
+  // =========================
+
+  setActiveSection(section: string): void {
+
+    this.activeSection = section;
+
+  }
+
+
+  // =========================
+  // DETECTA A SEÇÃO NA TELA
+  // =========================
+
+  @HostListener('window:scroll')
+
+  onScroll(): void {
+
+    const sections = [
+      'home',
+      'about',
+      'skills',
+      'projects',
+      'contact'
+    ];
+
+
+    const scrollPosition =
+      window.scrollY + 150;
+
+
+    for (const section of sections) {
+
+      const element =
+        document.getElementById(section);
+
+
+      if (!element) {
+        continue;
+      }
+
+
+      const top =
+        element.offsetTop;
+
+      const height =
+        element.offsetHeight;
+
+
+      if (
+        scrollPosition >= top &&
+        scrollPosition < top + height
+      ) {
+
+        this.activeSection = section;
+
+        break;
+
+      }
+
+    }
+
+  }
 
 }
